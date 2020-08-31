edde974aaecf8b82207f4fc581793a85b84d184f
Isak Horvath <48546805+tavro@users.noreply.github.com>
2020-08-31 17:36:58 +0200

    NEW: Printing numbers & expressions

--- lang.py
+++ lang.py
@@ -2,14 +2,28 @@ from sys import *
 
 tokens = []
 
+def print_line(o):
+	if o[0:6] == "STRING":
+		print(o[8:-1])
+	elif o[0:6] == "NUMBER" or o[0:6] == "EXPRES":
+		print(o[7:])
+
 def open_file(filename):
 	data = open(filename, "r").read()
+	data += "\n"
 	return data
 
 def lex(contents):
+
 	token = ""
-	state = 0
 	string = ""
+	expression = ""
+	number = ""
+
+	state = 0
+
+	is_expression = False
+	
 	contents = list(contents)
 	for c in contents:
 		token += c
@@ -17,9 +31,22 @@ def lex(contents):
 			if state == 0:
 				token = ""
 		elif token == "\n":
+			if expression != "" and is_expression:
+				tokens.append("EXPRES:" + expression)
+				expression = ""
+			elif expression != "" and not is_expression:
+				tokens.append("NUMBER:" + expression)
+				expression = ""
 			token = ""
 		elif token == ">":
 			tokens.append("PRINT")
+			token = ""
+		elif len(token) == 1 and token.isdigit():
+			expression += token
+			token = ""
+		elif token in "+-*/()":
+			is_expression = True
+			expression += token
 			token = ""
 		elif token == "\"":
 			if state == 0:
@@ -37,10 +64,9 @@ def lex(contents):
 def parse(token_list):
 	i = 0
 	while i < len(token_list):
-		if token_list[i] + " " + token_list[i+1][0:6] == "PRINT STRING":
-			print(token_list[i+1][7:])
+		if token_list[i] == "PRINT":
+			print_line(token_list[i+1])
 			i+=2
-
 
 def run():
 	data = open_file("program.coff")
